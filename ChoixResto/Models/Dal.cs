d1128fc2dcb6fa5f87c97dc15b1fd95be866be15
YHKero <yassine.khalifii@gmail.com>
2019-09-20 17:07:08 +0200

    first push

---
+++ ChoixResto/Models/Dal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Models
+{
+  public class Dal : IDal
+{
+    private DbContext bdd;
+
+    public Dal()
+    {
+        bdd = new DbContext();
+    }
+
+    public List<Resto> ObtientTousLesRestaurants()
+    {
+        return bdd.Restos.ToList();
+    }
+
+    public void Dispose()
+    {
+        bdd.Dispose();
+    }
+}
+}
---
+++ ChoixResto/Models/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChoixResto.Models
+{
+    public class DbContext
+    {
+        public DbSet<Sondage> Sondages { get; set; }
+        public DbSet<Resto> Restos { get; set; }
+
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
